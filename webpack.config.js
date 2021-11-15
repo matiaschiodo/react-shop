ade38f0dbe79018cc2998dd544f1d161839cfa23
matiaschiodo <matiaschiodo@gmail.com>
2021-11-15 11:52:12 -0300

    Netlify deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'react-shop/bundle.js',
-        publicPath: '/react-shop/',
+        filename: 'bundle.js',
+        publicPath: '/',
     },
     mode: 'development',
     resolve: {
