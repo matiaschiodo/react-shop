cd10db1c2fd5cd26942537c6c564268ebca86b8f
matiaschiodo <matiaschiodo@gmail.com>
2021-11-15 11:37:12 -0300

    Github pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
     return (
         <AppContext.Provider value={initialState}>
-            <BrowserRouter>
+            <BrowserRouter basename="/react-shop">
                 <Layout>
                     <Routes>
                         <Route
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
+        filename: 'react-shop/bundle.js',
+        publicPath: '/react-shop/',
     },
     mode: 'development',
     resolve: {
