8fe1df5841781ee6d2cc46b9d84d28afdc1c4888
matiaschiodo <matiaschiodo@gmail.com>
2021-11-15 12:43:54 -0300

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
         publicPath: '/',
     },
@@ -61,7 +61,7 @@ module.exports = {
         }),
     ],
     devServer: {
-        allowedHosts: path.join(__dirname, 'dist'),
+        allowedHosts: path.join(__dirname, 'build'),
         historyApiFallback: true,
         compress: true,
         port: 8080,
