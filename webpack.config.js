9b37bc1ecafa11268e4ffdb022fe0a06dd230554
matiaschiodo <matiaschiodo@gmail.com>
2021-11-15 12:48:49 -0300

    gh-pages deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: '/react-shop/bundle.js',
+        filename: 'react-shop/bundle.js',
         publicPath: '/react-shop',
     },
     mode: 'production',
